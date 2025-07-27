27eb8ad0cf941ae6374300092d96c293ea46a0c5
raja9112 <rrn.rajarajan@gmail.com>
2025-07-27 19:37:30 +0530

    Modified Models to reduce redanduncy and added abstract base code and added code to create wallet using ETH

--- wallet_core/wallets/models.py
+++ wallet_core/wallets/models.py
@@ -24,15 +24,36 @@ class Wallet(TimeStampMixin):
     wallet_address = models.CharField(max_length=255, unique=True)
     wallet_type = models.CharField(max_length=20, choices=WALLET_TYPE_CHOICES, default='custodial')
     storage_type = models.CharField(max_length=20, choices=STORAGE_TYPE_CHOICES, default='hot')
-    chain_symbol = models.CharField(max_length=20, null=True)
-    network_type = models.CharField(max_length=20, null=True)
+    chain = models.ForeignKey('BlockchainNetwork', on_delete=models.CASCADE, null=True)
     wallet_format = models.CharField(max_length=50, null=True)
     address_index = models.CharField(max_length=512, null=True)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         db_table = 'wallet'
         indexes = [
             models.Index(fields=['id', 'wallet_address'])
+        ]
+        constraints = [
+            models.UniqueConstraint(fields=['wallet_address', 'chain'], name='unique_wallet_on_chain')
+        ]
+
+
+class BlockchainNetwork(TimeStampMixin):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    chain_symbol = models.CharField(max_length=20, unique=True)
+    network = models.CharField(max_length=20, null=True)
+    chain_id = models.BigIntegerField(null=True)
+
+    class Meta:
+        db_table = 'blockchain_network'
+        indexes = [
+            models.Index(fields=['id', 'name']),
+            models.Index(fields=['chain_symbol'])
+        ]
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'chain_symbol'], name='unique_network_name_chain_symbol')
         ]
 
 
---
+++ wallet_core/wallets/services/evm_chain_service.py
@@ -0,0 +1,24 @@
+from web3 import Web3
+from .base import BaseWalletService
+
+class EVMWalletService(BaseWalletService):
+    # todo: Use IPCProvider or WebSocketProvider for better performance in Production
+    #       HTTPProvider is suitable for development and testing
+    def __init__(self, provider_url):
+        self.web3 = Web3(Web3.HTTPProvider(provider_url))
+
+    def create_wallet(self):
+        acct = self.web3.eth.account.create()
+        return {
+        "address": acct.address,
+        "private_key_bytes": acct.key,  # For AES encryption (custodial)
+        "private_key_hex": acct.key.hex(),  # For returning in non-custodial
+        "public_key_hex": acct._key_obj.public_key.to_hex(),  # Optional
+        "chain_id": self.web3.eth.chain_id,
+        "network_version": self.web3.net.version,
+        "account_type": type(acct).__name__,  # 'LocalAccount'
+        "key_obj": acct._key_obj,  # Full crypto object, optional
+        "_private_key": acct._private_key,  # Same as acct.key
+    }
+
+
